a6b353f32504029afbaa528ac74c0d531d2e057c
bradd.schick <bradd.schick@transunion.com>
2020-10-13 18:17:41 -0400

    more buttons than I know what to do with

--- buttons.py
+++ buttons.py
@@ -5,16 +5,16 @@ import time
 # for btn in buttons:
 #     # GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #     GPIO.add_event_detect(buttons.get(btn), GPIO.RISING, callback=btn)
+
+buttons = [26, 16, 20, 19]
+
 def callback(chnl):
     print(f"well {chnl} was called")
 
-
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(buttons, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.add_event_detect(26, GPIO.RISING, callback=callback)
 GPIO.add_event_detect(19, GPIO.RISING, callback=callback)
